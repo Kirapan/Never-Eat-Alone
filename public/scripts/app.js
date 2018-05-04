d6b2649f1a3e29b00d761e599aa7fda4e868c3f4
Kirapan <pxqpxqpxq@hotmail.com>
2018-05-04 15:43:15 +0000

    add routes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,17 @@
+const linkedin = require('./linkedin.js')
+
 $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {
+    for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+});
+
+linkedin.companySearch();
+linkedin.linkedIn();
+
 });
