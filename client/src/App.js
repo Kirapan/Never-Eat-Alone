f0c7723c361700d7b3fc24ff7915232a4d48fd7e
Kirapan <pxqpxqpxq@hotmail.com>
2018-05-15 17:56:42 +0000

    css for profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles/css/main.css'
 import Parent from './components/Parent'
 import './styles/css/geo-suggest.css'
+import './styles/css/user-profile.css'
 
 const App = (props) => (
   <Parent/>
